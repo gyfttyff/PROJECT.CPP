fc1e80baa2a674fa5e2fa68806443079c78de7e7
Danil <9139316075mok@gmail.com>
2026-03-12 01:36:54 +0700

    + Composition

---
+++ project.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#define A 100
+#define B 2
+
+using namespace std;
+
+int composition (int a, int b)
+{
+        return a*b;
+}
+
+int main()
+{
+        cout << composition (A,B) << endl;
+}
+
+
